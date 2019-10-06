523205a6f8a4c060d946009d6cb0bf00b4973a4a
Bart Moorman <github@id10t.us>
2019-10-06 11:06:36 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+## This is designed for a Raspberry Pi
+
+### Docker Run
+```
+docker run \
+--detach \
+--name doorbell \
+--privileged \
+--env "SYNO_URL=http://**sub.do.main**:5000" \
+--env "SYNO_ACCOUNT=doorbell" \
+--env "SYNO_PASSWD=scQWntLwuSWKpPsK" \
+--env "SYNO_CAMERA_ID=12" \
+--env "SYNO_CAMERA_STREAM=1" \
+--env "PUSHOVER_USERS=alias:token:user_or_group_key:priority" \
+bmoorman/doorbell:latest
+```
+
+### Docker Compose
+```
+version: "3.7"
+services:
+  doorbell:
+    image: bmoorman/doorbell:latest
+    container_name: doorbell
+    environment:
+      - SYNO_URL=http://**sub.do.main**:5000
+      - SYNO_ACCOUNT=doorbell
+      - SYNO_PASSWD=scQWntLwuSWKpPsK
+      - SYNO_CAMERA_ID=12
+      - SYNO_CAMERA_STREAM=1
+      - PUSHOVER_USERS=alias:token:user_or_group_key:priority
+    privileged: true
+```
---
+++ bin/doorbell.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import os, requests
+from datetime import datetime
+from time import sleep
+
+buttonPin = os.getenv('BUTTON_PIN')
+pressLength = 100 / 1000
+
+synoURL = os.getenv('SYNO_URL')
+synoAccount = os.getenv('SYNO_ACCOUNT')
+synoPasswd = os.getenv('SYNO_PASSWD')
+synoCameraId = os.getenv('SYNO_CAMERA_ID')
+synoCameraStream = os.getenv('SYNO_CAMERA_STREAM')
+
+pushUsers = os.getenv('PUSHOVER_USERS')
+
+print('{0} - Setting up GPIO'.format(datetime.now()))
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buttonPin, GPIO.IN)
+
+print('{0} - Setting up request sessions'.format(datetime.now()))
+synoSession = requests.Session()
+pushSession = requests.Session()
+
+try:
+  print('{0} - Startup complete'.format(datetime.now()))
+
+  while True:
+    if GPIO.wait_for_edge(buttonPin, GPIO.RISING, timeout=5000):
+      print('{0} - Rising edge detected'.format(datetime.now()))
+      sleep(pressLength)
+
+      if GPIO.input(buttonPin) == GPIO.HIGH:
+        print('{0} - Button was pressed'.format(datetime.now()))
+
+        try:
+          print('{0} - Logging in to Surveillance Station'.format(datetime.now()))
+          synoSession.post('{0}/webapi/auth.cgi'.format(synoURL), data={'api': 'SYNO.API.Auth', 'method': 'Login', 'version': 6, 'account': synoAccount, 'passwd': synoPasswd, 'session': 'SurveillanceStation'}, timeout=2.5)
+
+          print('{0} - Retrieving snapshot from Surveillance Station'.format(datetime.now()))
+          request = synoSession.post('{0}/webapi/entry.cgi'.format(synoURL), data={'api': 'SYNO.SurveillanceStation.Camera', 'method': 'GetSnapshot', 'version': '8', 'cameraId': synoCameraId, 'camStm': synoCameraStream}, timeout=2.5)
+        except:
+          request = None
+
+        for pushUser in pushUsers.split(','):
+          pushUser = pushUser.split(':')
+          print('{0} - Sending notification to {1}'.format(datetime.now(), pushUser[0]))
+
+          if request:
+            pushSession.post('https://api.pushover.net/1/messages.json', data={'token': pushUser[1], 'user': pushUser[2], 'priority': pushUser[3], 'message': 'Someone is at the door!'}, files={'attachment': request.content}, timeout=5.0)
+          else:
+            pushSession.post('https://api.pushover.net/1/messages.json', data={'token': pushUser[1], 'user': pushUser[2], 'priority': pushUser[3], 'message': 'Someone is at the door!\n[Preview unavailable]'}, timeout=5.0)
+      else:
+        print('{0} - Ignoring; this was probably interference'.format(datetime.now()))
+finally:
+  print('{0} - Cleaning up GPIO'.format(datetime.now()))
+  GPIO.cleanup()
